ca4277ecea18693261c4b986ed222b91823d53fc
Tanjil <zzhasanzz246@gmail.com>
2024-06-11 23:53:30 +0600

    updated server.js

--- server.js
+++ server.js
@@ -6,42 +6,47 @@ const cors = require('cors');
 const formatMessage = require('./utils/messages');
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
-//userJoin object => id, username, room
-//getCurrentUser => function to find user according to username
-
+// Create Express app
 const app = express();
 
-app.use(
-    cors ({
-        origin: '*',
-    })
-)
+// Enable CORS for all origins
+app.use(cors({
+    origin: '*',
+}));
 
-const botName =  'streamON Bot';
-//set static folder
+const botName = 'streamON Bot';
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(3000, function(){
-    console.log('listening for requests on port 3000,');
+// Create HTTP server
+const server = http.createServer(app);
+
+// Initialize socket.io with CORS configuration
+const io = socketio(server, {
+    cors: {
+        origin: ["https://stream-on-flax.vercel.app"],
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true,
+    }
 });
 
-var io = socketio(server);
-
-//run when client connects
+// Run when client connects
 io.on('connection', (socket) => {
     socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room );
+        const user = userJoin(socket.id, username, room);
         socket.join(user.room);
 
-        //Welcome current user
+        // Welcome current user
         socket.emit('message', formatMessage(botName, 'Welcome to streamON!'));
 
-        //Broadcast when a user connects
+        // Broadcast when a user connects
         socket.broadcast
-        .to(user.room)
-        .emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+            .to(user.room)
+            .emit('message', formatMessage(botName, `${user.username} has joined the chat`));
 
-        //Send users and room information
+        // Send users and room information
         io.to(user.room).emit('roomUsers', {
             room: user.room,
             users: getRoomUsers(user.room)
@@ -52,15 +57,15 @@ io.on('connection', (socket) => {
                 const formData = new FormData();
                 const file = new File([fileData.data], fileData.name, { type: fileData.type });
                 formData.append('file', file);
-        
+
                 const response = await fetch("https://file.io/?expires=1d", {
                     method: "POST",
                     body: formData
                 });
-        
+
                 const data = await response.json();
                 console.log(data);
-        
+
                 if (response.ok) {
                     io.to(user.room).emit('fileShared', { username: user.username, fileUrl: data.link, fileName: fileData.name });
                 } else {
@@ -70,7 +75,7 @@ io.on('connection', (socket) => {
                 console.error(error);
                 socket.emit('message', formatMessage(botName, 'File upload failed.'));
             }
-        });        
+        });
     });
 
     socket.on('playerControl', (data) => {
@@ -81,25 +86,22 @@ io.on('connection', (socket) => {
         io.to(data.roomCode).emit('audioCommand', { message: data.message, context: data.context });
     });
 
-    socket.on('pomodoroControl', data => {
+    socket.on('pomodoroControl', (data) => {
         io.to(data.roomCode).emit('pomodoroCommand', { action: data.action, timeLeft: data.timeLeft });
     });
 
-
-    // console.log('made socket connection', socket.id);
-
-    //Listen for chatMessage
+    // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
         const user = getCurrentUser(socket.id);
         io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
 
-    //Runs when client disconnects
+    // Runs when client disconnects
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
 
         if (user) {
-            io.to(user.room).emit('message',formatMessage(botName, `${user.username} has left the chat`));
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
 
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
@@ -107,4 +109,9 @@ io.on('connection', (socket) => {
             });
         }
     });
-}); 
+});
+
+// Start server
+server.listen(3000, function () {
+    console.log("server listening on port 3000");
+});
